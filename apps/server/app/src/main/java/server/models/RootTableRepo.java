4416b92ac371f5d1247a809c51f1b673e32ea529
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-12-03 15:09:39 +0200

    🪾 java_models => 🛠️ _

--- apps/server/app/src/main/java/server/models/RootTableRepo.java
+++ apps/server/app/src/main/java/server/models/RootTableRepo.java
@@ -2,16 +2,13 @@ package server.models;
 
 import java.util.UUID;
 
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 
 import reactor.core.publisher.Mono;
-import server.decorators.core.ErrAPI;
 
-@Repository
+@NoRepositoryBean
 public interface RootTableRepo<T> extends ReactiveCrudRepository<T, UUID> {
 
-  default Mono<T> insert(T entity) {
-    return Mono.error(new ErrAPI("insert method must be implemented", 400));
-  }
+  public abstract Mono<T> insert(T arg);
 }
